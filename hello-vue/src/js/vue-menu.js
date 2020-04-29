be0f88477db0426f37a7c17d1b5a676510ab4061
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-04-29 02:09:04 -0500

    [add] vue basics

---
+++ hello-vue/src/js/vue-menu.js
@@ -0,0 +1,23 @@
+Vue.component('vue-menu', {
+  props: ['ecosystem'],
+  template:
+    `<nav>
+      <ul>
+        <li v-for='(techItem) in ecosystem'>
+          <a v-bind:href="techItem.link">{{techItem.title}}</a>
+        </li>
+      </ul>
+    </nav>`
+})
+new Vue({
+  el: '#navbar',
+  data: {
+    vueEcosystem: [
+      { id: 0, title: 'Home', link: './index.html' },
+      { id: 1, title: 'Vue CDN', link: './1-vue-cdn.html' },
+      { id: 2, title: 'Vue If', link: './2-vue-if.html' },
+      { id: 3, title: 'Vue For', link: './3-vue-for.html' },
+      { id: 4, title: 'Vue For (exercise)', link: './3.1-vue-for-exercise.html' },
+    ]
+  }
+})
