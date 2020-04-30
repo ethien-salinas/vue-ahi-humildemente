b0cd1ec6f8d313abcfa4142c4fa29c3f41dcbdae
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-04-30 00:51:12 -0500

    [add] components and methods

--- hello-vue/src/js/vue-menu.js
+++ hello-vue/src/js/vue-menu.js
@@ -18,6 +18,8 @@ new Vue({
       { id: 2, title: 'Vue If', link: './2-vue-if.html' },
       { id: 3, title: 'Vue For', link: './3-vue-for.html' },
       { id: 4, title: 'Vue For (exercise)', link: './3.1-vue-for-exercise.html' },
+      { id: 5, title: 'Vue components', link: './4-vue-component.html' },
+      { id: 6, title: 'Vue methods', link: './5-vue-methods.html' },
     ]
   }
 })
